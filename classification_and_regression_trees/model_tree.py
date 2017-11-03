69549a71c4c2f9cbef37ae7f88a4db3568f43afa
pytlab <shaozhengjiang@gmail.com>
2017-11-03 15:04:41 +0800

    添加模型树绘制

--- classification_and_regression_trees/model_tree.py
+++ classification_and_regression_trees/model_tree.py
@@ -31,6 +31,55 @@ def ferr(dataset):
     y_prime = X*w
     return np.var(y_prime - y)
 
+def get_nodes_edges(tree, root_node=None):
+    ''' 返回树中所有节点和边
+    '''
+    Node = namedtuple('Node', ['id', 'label'])
+    Edge = namedtuple('Edge', ['start', 'end'])
+
+    nodes, edges = [], []
+
+    if type(tree) is not dict:
+        return nodes, edges
+
+    if root_node is None:
+        label = '{}: {}'.format(tree['feat_idx'], tree['feat_val'])
+        root_node = Node._make([uuid.uuid4(), label])
+        nodes.append(root_node)
+
+    for sub_tree in (tree['left'], tree['right']):
+        if type(sub_tree) is dict:
+            node_label = '{}: {}'.format(sub_tree['feat_idx'], sub_tree['feat_val'])
+        else:
+            node_label = '{}'.format(np.array(sub_tree.T).tolist()[0])
+        sub_node = Node._make([uuid.uuid4(), node_label])
+        nodes.append(sub_node)
+
+        edge = Edge._make([root_node, sub_node])
+        edges.append(edge)
+
+        sub_nodes, sub_edges = get_nodes_edges(sub_tree, root_node=sub_node)
+        nodes.extend(sub_nodes)
+        edges.extend(sub_edges)
+
+    return nodes, edges
+
+def dotify(tree):
+    ''' 获取树的Graphviz Dot文件的内容
+    '''
+    content = 'digraph decision_tree {\n'
+    nodes, edges = get_nodes_edges(tree)
+
+    for node in nodes:
+        content += '    "{}" [label="{}"];\n'.format(node.id, node.label)
+
+    for edge in edges:
+        start, end = edge.start, edge.end
+        content += '    "{}" -> "{}";\n'.format(start.id, end.id)
+    content += '}'
+
+    return content
+
 def tree_predict(data, tree):
     if type(tree) is not dict:
         w = tree
@@ -46,6 +95,10 @@ def tree_predict(data, tree):
 if '__main__' == __name__:
     dataset = load_data('exp2.txt')
     tree = create_tree(dataset, fleaf, ferr, opt={'err_tolerance': 0.1, 'n_tolerance': 4})
+
+    # 生成模型树dot文件
+    with open('exp2.dot', 'w') as f:
+        f.write(dotify(tree))
 
     dataset = np.array(dataset)
     # 绘制散点图
